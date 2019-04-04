a251747ca40152a1e3851466f454c85c1d44471c
Taylor Rogers <taylor.rogers71@gmail.com>
2019-04-04 16:19:31 -0700

    Initial commit

---
+++ insert-branchbuild-link.js
@@ -0,0 +1,36 @@
+// ==UserScript==
+// @name         Branchbuild Link
+// @namespace    http://testsite.emsicc-qa.com/
+// @version      0.1
+// @description  Create a link for the branchbuild for a pull request
+// @author       Taylor Rogers
+// @match        https://github.com/emsiengineering/cc-frontend/pull/*
+// @require      https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js
+// @grant        none
+// ==/UserScript==
+'use strict';
+
+const createBranchBuildLink = () => {
+  $('span.commit-ref.css-truncate.user-select-contain.expandable.head-ref').each((index, el) => {
+    if (index === 0) {
+      $el = $(el);
+      let div = $('<div></div>');
+      const url = `http://testsite.${$el[0].title.replace(
+        'emsiengineering/cc-frontend:',
+        ''
+      )}.emsicc-qa.com`;
+      $a = $('<a href="' + url + '" />')
+        .text('Go to Branchbuild')
+        .attr('target', '_blank')
+        .attr('rel', 'noopener noreferrer')
+        .addClass('branchbuild-link')
+        .css('display', 'block');
+      div.append($a);
+      $('svg.octicon.octicon-clippy.d-inline-block.mx-1.js-clipboard-clippy-icon').after(div);
+    }
+  });
+};
+
+$(document).ready(function() {
+  createBranchBuildLink();
+});
